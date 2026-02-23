c0c760568a9b7249ab30bbae48238d737d3e6e4c
Arshdeep Kaur <arshbajwa0304@gmail.com>
2026-02-23 10:33:30 +0000

    Updated home view

--- core/views.py
+++ core/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .models import Doctor, Patient, Appointment
 
 def home(request):
-    return render(request, 'core/home.html')
+    total_doctors = Doctor.objects.count()
+    total_patients = Patient.objects.count()
+    total_appointments = Appointment.objects.count()
+    pending_appointments = Appointment.objects.filter(status='Pending').count()
+
+    context = {
+        'total_doctors': total_doctors,
+        'total_patients': total_patients,
+        'total_appointments': total_appointments,
+        'pending_appointments': pending_appointments,
+    }
+
+    return render(request, 'core/home.html', context)
